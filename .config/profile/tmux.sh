3a8121a395c19882339ad977245c8b55661b8c06
_luwum <luwum@pm.me>
2018-01-30 11:54:32 -0600

    tmux initialization is more verbose

--- .config/profile/tmux.sh
+++ .config/profile/tmux.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
-
 #  Location for TMUX PLUGIN MANAGER
 set_env_var "TMUX_PLUGIN_MANAGER_PATH" "${XDG_CONFIG_HOME}/tmux/plugins/tpm"
 
 # Check if current shell is running inside TMUX
 if [ -z ${TMUX:-}; then
     # Create a new session called `$USER`, attach if it already exists.
-    tmux attach || tmux new -A -D -s $USER
+    if [ -z $(tmux has-session -t $USER) ]; then
+        tmux attach -t $USER
+    else
+        tmux new -s $USER
 fi
