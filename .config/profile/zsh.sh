c482e31668c52bb5dc5ec5fc6cd659271121cd10
_luwum <luwum@pm.me>
2018-01-31 15:09:11 -0600

    zsh fixup

--- .config/profile/zsh.sh
+++ .config/profile/zsh.sh
@@ -1,2 +1,11 @@
 # ZSH startup/shutdown files
 set_env_var "ZDOTDIR" "${XDG_CONFIG_HOME}/zsh"
+
+# Location of OH-MY-ZSH
+set_env_var "ZSH" "${ZDOTDIR}/oh-my-zsh"
+set_env_var "ZSH_THEME" "agnoster"
+
+if [ ! -d ${ZSH} ]; then
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ${ZSH}
+fi
+
--- .config/zsh/.zprofile
+++
@@ -1,9 +0,0 @@
-# Location of OH-MY-ZSH
-set_env_var "ZSH" "${XDG_CONFIG_HOME}/zsh/oh-my-zsh"
-set_env_var "ZSH_THEME" "agnoster"
-
-if [ ! -d ${ZSH} ]; then
-    git clone git://github.com/robbyrussell/oh-my-zsh.git ${ZSH}
-fi
-
-
