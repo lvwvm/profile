46f40d500d3c2cb78c491dd4f8d96f8d40ed7c25
_luwum <luwum@pm.me>
2018-02-01 23:35:58 -0600

    nvm moved to XDG_DATA_HOME

--- .config/profile/nvm.sh
+++ .config/profile/nvm.sh
@@ -1,8 +1,8 @@
-if [ ! -d "${XDG_CONFIG_HOME}/nvm" ]; then
-    mkdir -p ${XDG_CONFIG_HOME}/nvm
+if [ ! -d "${XDG_DATA_HOME}/nvm" ]; then
+    mkdir ${XDG_DATA_HOME}/nvm
 fi
 
-set_env_var "NVM_DIR" "${XDG_CONFIG_HOME}/nvm"
+set_env_var "NVM_DIR" "${XDG_DATA_HOME}/nvm"
 
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
