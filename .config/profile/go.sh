a5f0980e6132d2273356e7606a4da451d6657912
_luwum <luwum@pm.me>
2018-02-06 11:55:26 -0600

    gvm environment variables

--- .config/profile/go.sh
+++ .config/profile/go.sh
@@ -1,5 +1,6 @@
 # Go Environment Variables
-
 set_env_var "GOPATH" "${XDG_DATA_HOME}/go"
 set_env_var "GOBIN" "${GOPATH}/bin"
 
+# GVM Environment Variables
+set_env_var "GVM_ROOT" "${XDG_DATA_HOME}/gvm"
