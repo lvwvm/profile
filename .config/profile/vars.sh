d98446bfb0677ecdfd0c004b8999105983cf599d
_luwum <luwum@pm.me>
2018-03-12 13:09:49 -0500

    PROFILE_HOME changed to profile.

--- .config/profile/vars.sh
+++ .config/profile/vars.sh
@@ -4,4 +4,4 @@ set_env_var "LANG"  "en_US.UTF-8"
 set_env_var "EDITOR" "$(command -v nvim)"
 set_env_var "PAGER" "$(command -v less)"
 
-set_env_var "PROFILE_HOME" "${XDG_CONFIG_HOME}/profile"
+set_env_var "PROFILE" "${XDG_CONFIG_HOME}/profile"
