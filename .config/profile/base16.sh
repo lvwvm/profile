c0d26fb413203f476e3468cc0dd08273ec079c12
_luwum <luwum@pm.me>
2018-03-24 16:01:48 -0500

    set BASE16_SHELL variable using set_env_var

--- .config/profile/base16.sh
+++ .config/profile/base16.sh
@@ -1,4 +1,4 @@
-BASE16_SHELL=${XDG_CONFIG_HOME}/base16-shell
+set_env_var "BASE16_SHELL" "${XDG_CONFIG_HOME}/base16-shell"
 if [ ! -d "${BASE16_SHELL}" ]; then
     git clone https://github.com/chriskempson/base16-shell.git "${BASE16_SHELL}"
 fi
