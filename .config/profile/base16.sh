bfebc151bd069043d31183d3ac25452d486c9631
_luwum <luwum@pm.me>
2018-03-24 16:41:13 -0500

    making coding styling more consistent.

--- .config/profile/base16.sh
+++ .config/profile/base16.sh
@@ -1,7 +1,9 @@
 set_env_var "BASE16_SHELL" "${XDG_CONFIG_HOME}/base16-shell"
+
 if [ ! -d "${BASE16_SHELL}" ]; then
     git clone https://github.com/chriskempson/base16-shell.git "${BASE16_SHELL}"
 fi
+
 [ -n "${PS1}" ] \
     && [ -s "${BASE16_SHELL}/profile_helper.sh" ] \
     && eval "$("${BASE16_SHELL}/profile_helper.sh")"
--- .config/profile/nvm.sh
+++ .config/profile/nvm.sh
@@ -1,8 +1,8 @@
-if [ ! -d "${XDG_DATA_HOME}/nvm" ]; then
-    mkdir ${XDG_DATA_HOME}/nvm
-fi
-
 set_env_var "NVM_DIR" "${XDG_DATA_HOME}/nvm"
 
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+if [ ! -d "${NVM_DIR}" ]; then
+    mkdir "${NVM_DIR}"
+fi
+
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
