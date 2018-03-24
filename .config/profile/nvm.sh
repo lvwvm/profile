575dddd077f4d6990753ddefad5ac627f4ad606b
_luwum <luwum@pm.me>
2018-03-24 17:36:28 -0500

    better variable interpolation

--- .config/profile/nvm.sh
+++ .config/profile/nvm.sh
@@ -4,5 +4,5 @@ if [ ! -d "${NVM_DIR}" ]; then
     mkdir "${NVM_DIR}"
 fi
 
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"  # This loads nvm
+[ -s "${NVM_DIR}/bash_completion" ] && . "${NVM_DIR}/bash_completion"  # This loads nvm bash_completion
