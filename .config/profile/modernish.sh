9edce29453b565a4b9306c3b958f71289024af83
_luwum <luwum@pm.me>
2018-03-30 22:07:40 -0500

    feature: (plugin) Basic Modernish installation.

---
+++ .config/profile/modernish.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set_env_var "MODERNISH_HOME" "${XDG_DATA_HOME}/modernish"
+
+if ! [ -d "${MODERNISH_HOME}" ]; then
+     git clone https://github.com/modernish/modernish ${MODERNISH_HOME} 
+fi
+
