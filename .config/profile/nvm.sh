d45ae6ddf613f9f6c77cc34e8c33464d17dfa73c
_luwum <luwum@pm.me>
2018-04-03 23:42:35 -0500

    refactor: (ext) replaced mkdir with git clone

--- .config/profile/nvm.sh
+++ .config/profile/nvm.sh
@@ -2,9 +2,9 @@
 
 set_env_var "NVM_DIR" "${XDG_DATA_HOME}/nvm"
 
-if [ ! -d "${NVM_DIR}" ]; then
-    mkdir "${NVM_DIR}"
+if ! [ -d "${NVM_DIR}" ]; then
+    git clone https://github.com/creationix/nvm "${NVM_DIR}"
 fi
 
-[ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"  # This loads nvm
-[ -s "${NVM_DIR}/bash_completion" ] && . "${NVM_DIR}/bash_completion"  # This loads nvm bash_completion
+[ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"  # This loads nvm
+[ -s "${NVM_DIR}/bash_completion" ] && \. "${NVM_DIR}/bash_completion"  # This loads nvm bash_completion
