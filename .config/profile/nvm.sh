e9b3e2f34bfcc8e36f8cf4a53a38495e95dd6ef9
_luwum <luwum@pm.me>
2018-04-04 18:57:23 -0500

    style: unescape source command

--- .config/profile/nvm.sh
+++ .config/profile/nvm.sh
@@ -6,5 +6,5 @@ if ! [ -d "${NVM_DIR}" ]; then
     git clone https://github.com/creationix/nvm "${NVM_DIR}"
 fi
 
-[ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"  # This loads nvm
-[ -s "${NVM_DIR}/bash_completion" ] && \. "${NVM_DIR}/bash_completion"  # This loads nvm bash_completion
+[ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"  # This loads nvm
+[ -s "${NVM_DIR}/bash_completion" ] && . "${NVM_DIR}/bash_completion"  # This loads nvm bash_completion
