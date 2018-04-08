c1ebf9d5d8bdc0e13e1c0512f034f3a84af9a4f0
_luwum <luwum@pm.me>
2018-04-08 14:30:10 -0500

    fix: GOBIN move to XDG_BIN_HOME

--- .config/profile/go.sh
+++ .config/profile/go.sh
@@ -2,7 +2,7 @@
 
 # Go Environment Variables
 set_env_var "GOPATH" "${XDG_DATA_HOME}/go"
-set_env_var "GOBIN" "${GOPATH}/bin"
+set_env_var "GOBIN" "${XDG_BIN_HOME}"
 
 # GVM Environment Variables
 set_env_var "GVM_ROOT" "${XDG_DATA_HOME}/gvm"
