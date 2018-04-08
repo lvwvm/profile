8fc8221753649caccee42c98d8f8a005902b3ab9
_luwum <luwum@pm.me>
2018-04-07 19:19:12 -0500

    fix: explicitly set zsh history variables

--- .config/profile/zsh.sh
+++ .config/profile/zsh.sh
@@ -9,6 +9,15 @@ set_env_var "ZPLUG_HOME" "${XDG_DATA_HOME}/zplug"
 # All binaries go here.
 set_env_var "ZPLUG_BIN" "${XDG_BIN_HOME}"
 
+# The file to save the history in when an interactive shell exits.
+set_env_var "HISTFILE" "${XDG_DATA_HOME}/zsh/.histfile"
+
+# The maximum number of events stored in the internal history list.
+set_env_var "HISTSIZE" "10000"
+
+# The maximum number of history events to save in the history file.
+save_env_var "SAVEHIST" "10000"
+
 if ! [ -d "${ZPLUG_HOME}" ]; then
     git clone https://github.com/zplug/zplug --depth=1 --branch 2.4.2 "${ZPLUG_HOME}"
 fi
