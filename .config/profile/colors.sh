fb4e5f3f932254aeeeafa9c6e0f32aa4a23bfcc7
_luwum <luwum@pm.me>
2018-05-03 13:03:26 -0500

    refactor: moved setting of shell colors to zshrc

--- .config/profile/colors.sh
+++ .config/profile/colors.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
 # Check for a syslink to the vimspectr plugin. Create one if necessary.
-if [ ! -L ~/.vimspectr_shell ]; then
+if [ ! -L "$HOME/.vimspectr_shell" ]; then
     if [ -d "$XDG_DATA_HOME/nvim/plugged/vimspectr/shell" ]; then
-        ln -s ~/.local/share/nvim/plugged/vimspectr/shell ~/.vimspectr-shell
-    for f in ~/.vimspectr-shell/*-*; do
-        ln -s "$f" "$( \
-            echo "$f.sh" | \
-            sed s#vimspectr-shell/#config/zsh/plugins/chriskempson/base16-shell/scripts/base16-#)"
-    done
+        ln -s "$XDG_DATA_HOME/nvim/plugged/vimspectr/shell" "$HOME/.vimspectr-shell"
+        for f in $HOME/.vimspectr-shell/*-*; do
+            ln -s "$f" "$( \
+                echo "$f.sh" | \
+                sed s#vimspectr-shell/#config/zsh/plugins/chriskempson/base16-shell/scripts/base16-#)"
+        done
     fi
 fi
 
@@ -17,5 +17,3 @@ if [ ! -d "$XDG_DATA_HOME/LS_COLORS" ];then
     git clone https://github.com/trapd00r/LS_COLORS "$XDG_DATA_HOME/LS_COLORS" 
     ln -s ~/.dir_colors "$XDG_DATA_HOME/LS_COLORS/LS_COLORS"
 fi
-
-[ -n "$PS1" ] && . "$HOME/.vimspectr-shell/vimspectr240-dark" 
