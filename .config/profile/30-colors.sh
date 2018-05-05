cf096b15373355ce1c7f7e5192742640897694eb
_luwum <luwum@pm.me>
2018-05-04 21:54:49 -0500

    typo: find the correct vimspectr-shell link

--- .config/profile/30-colors.sh
+++ .config/profile/30-colors.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Check for a syslink to the vimspectr plugin. Create one if necessary.
-if [ ! -L "$HOME/.vimspectr_shell" ]; then
+if [ ! -L "$HOME/.vimspectr-shell" ]; then
     if [ -d "$XDG_DATA_HOME/nvim/plugged/vimspectr/shell" ]; then
         ln -s "$XDG_DATA_HOME/nvim/plugged/vimspectr/shell" "$HOME/.vimspectr-shell"
         for f in $HOME/.vimspectr-shell/*-*; do
