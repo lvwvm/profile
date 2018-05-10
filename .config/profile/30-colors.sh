1f0ab1d73673865199418c11f17bf0b31f48ca9e
_luwum <luwum@pm.me>
2018-05-09 21:31:55 -0500

    chore: use base16_phd as shell colorscheme

--- .config/profile/30-colors.sh
+++ .config/profile/30-colors.sh
@@ -17,3 +17,5 @@ if [ ! -d "$XDG_DATA_HOME/LS_COLORS" ];then
     git clone https://github.com/trapd00r/LS_COLORS "$XDG_DATA_HOME/LS_COLORS" 
     ln -s ~/.dir_colors "$XDG_DATA_HOME/LS_COLORS/LS_COLORS"
 fi
+
+base16_phd
