9429459b4711b3bdacdac5f72355d48957ed8d76
_luwum <luwum@pm.me>
2019-03-21 14:27:52 -0500

    regression: Removed modernish and asdf.

--- .config/profile/21-modernish.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-set_env_var "MODERNISH_HOME" "${XDG_DATA_HOME}/modernish"
-
-if ! [ -d "${MODERNISH_HOME}" ]; then
-     git clone https://github.com/modernish/modernish "${MODERNISH_HOME}" 
-fi
-
--- .config/profile/40-asdf.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-#ASDF is an Extendable Version Manager. 
-set_env_var "ASDF_CONFIG_FILE" "${XDG_CONFIG_HOME}/asdf/.asdfrc" 
