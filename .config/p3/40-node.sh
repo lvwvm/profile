524719efcec3e3d7b1fa2b3450167fd4ce7fcdaf
_luwum <luwum@pm.me>
2019-06-20 11:18:35 -0500

    feat: NVM plugin and enviroment variables.

--- .config/p3/40-node.sh
+++ .config/p3/40-node.sh
@@ -11,7 +11,8 @@ fi
 if [ ! -d "${XDG_CACHE_HOME}/npm" ]; then
     mkdir "${XDG_CACHE_HOME}/npm" 
 fi
-
+var "NVM_DIR" "${XDG_DATA_HOME}/nvm"
+var "NVM_LAZY_LOAD" "true"
 var "NPM_CONFIG_USERCONFIG" "${XDG_CONFIG_HOME}/npm/npmrc"
 var "NPM_CONFIG_INIT_MODULE" "${XDG_DATA_HOME}/npm/npm-init.js"
 var "NPM_CONFIG_CACHE" "${XDG_CACHE_HOME}/npm"
