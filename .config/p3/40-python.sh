e126428c77fec2017e73af510187f5ea4a5ea8e4
_luwum <luwum@pm.me>
2019-11-13 17:07:18 -0600

    chore(p3,python): pyenv and virtualenv setup.

--- .config/p3/40-python.sh
+++ .config/p3/40-python.sh
@@ -4,14 +4,22 @@ var "PYENV_ROOT" "${XDG_DATA_HOME}/pyenv"
 
 # pyenv manages python versions.
 if [ ! -d "${PYENV_ROOT}" ]; then
-    git clone https://github.com/pyenv/pyenv.git "${PYENV_ROOT}"
+    curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+    rm "${PYENV_ROOT}/versions"
 fi
 
 # pyenv virtualenv support plugin.
-if [ ! -d "${PYENV_ROOT}/plugins/pyenv" ]; then
+if [ ! -d "${PYENV_ROOT}/plugins/pyenv-virtualenv" ]; then
     git clone https://github.com/pyenv/pyenv-virtualenv.git "${PYENV_ROOT}/plugins/pyenv-virtualenv"
 fi
 
 if [ ! -d "${WORKON_HOME}" ]; then
-    mkdir "$WORKON_HOME"
+    mkdir "${WORKON_HOME}"
 fi
+
+if [ ! -L "${PYENV_ROOT}/versions" ];then
+    ln -s "${PYENV_ROOT}/versions" "${WORKON_HOME}"
+fi
+
+PATH="${PYENV_ROOT}/shims:$PATH"
+export PATH
---
+++ .zshenv
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+# source pyenv
+eval "$(pyenv init -)" 
+# source pyenv virtualenv
+eval "$(pyenv virtualenv-init -)"
+
