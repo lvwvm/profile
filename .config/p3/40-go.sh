f41666acf6f52797292038f2d89af686d641d670
_luwum <luwum@pm.me>
2020-08-05 12:28:50 -0500

    feat(p3,go): GVM added to XDG_BIN_HOME

--- .config/p3/40-go.sh
+++ .config/p3/40-go.sh
@@ -6,3 +6,11 @@ var "GOBIN" "${XDG_BIN_HOME}"
 
 # GVM Environment Variables
 var "GVM_ROOT" "${XDG_DATA_HOME}/gvm"
+
+if [ ! -d "${GVM_ROOT}" ]; then
+	git clone https://github.com/moovweb/gvm "${GVM_ROOT}"
+fi
+
+if [ ! -h "${XDG_BIN_HOME}/gvm" ]; then
+	ln -sT "${GVM_ROOT}/bin/gvm" "${XDG_BIN_HOME}/gvm"
+fi
