f23481eafe9e0d95162438d72203c85f922405b6
_luwum <luwum@pm.me>
2020-08-12 13:56:52 -0500

    fix(p3,doom): Doom data directory moved.

--- .config/p3/99-doom.sh
+++ .config/p3/99-doom.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
-var "DOOMDIR" "$XDG_DATA_HOME/doom"
+var "DOOMDIR" "${XDG_CONFIG_HOME}/doom"
+var "DOOM_DATA_DIR" "${XDG_DATA_HOMR}/doom"
 
-if [ ! -d "${DOOMDIR}" ]; then
-	git clone https://github.com/hlissner/doom-emacs "${DOOMDIR}"
+# DOOM's data directory
+if [ ! -d "${DOOM_DATA_DIR}" ]; then
+	git clone https://github.com/hlissner/doom-emacs "${DOOM_DATA_DIR}"
 fi
 
 if [ ! -h "$XDG_BIN_HOME/doom" ]; then
-	ln -s "${DOOMDIR}/bin/doom" "$XDG_BIN_HOME"
+	ln -sT "${DOOM_DATA_DIR}/bin/doom" "$XDG_BIN_HOME"
 fi
