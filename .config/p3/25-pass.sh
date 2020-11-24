d11dee545542a9dbe17e52f947551580ad519060
lvwvm <luwum@pm.me>
2020-11-24 12:32:17 -0600

    chore(pass): expand and set pass environment vars.

--- .config/p3/25-pass.sh
+++ .config/p3/25-pass.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env sh
-var "PASSWORD_STORE_DIR" "$XDH/pass"
+
+# PASSWORD_STORE_DIR - Overrides the default password storage directory.
+var "PASSWORD_STORE_DIR" "$XDG_DATA_HOME/pass"
+[ ! -d "$XDG_DATA_HOME/pass" ] && mkdir "$XDG_DATA_HOME/pass"
+
+# PASSWORD_STORE_ENABLE_EXTENSIONS - This environment variable must be set to
+# "true" for extensions to be enabled.
+var "PASSWORD_STORE_ENABLE_EXTENSIONS" "true"
+
+# PASSWORD_STORE_EXTENSIONS_DIR - The location to look for executable extension
+# files, by default PASSWORD_STORE_DIR/.extensions.
+var "PASSWORD_STORE_EXTENSIONS_DIR" "$PASSWORD_STORE_DIR/ext"
+[ ! -d "$PASSWORD_STORE_DIR/ext" ] && mkdir "$PASSWORD_STORE_DIR/ext"
