0935eb0dca1f4c69315936dab8871b00cbfc8bfd
lvwvm <luwum@pm.me>
2020-11-24 11:47:55 -0600

    style(gnupg): expand shorthand variables.

--- .config/p3/20-gnupg.sh
+++ .config/p3/20-gnupg.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
-var "GNUPGHOME" "${XDH}/gnupg"
-var "GNUPG_AGENT_CONF" "${XCH}/gnupg/gpg-agent.conf"
-var "GNUPG_CONF" "${XCH}/gnupg/gpg.conf"
+var "GNUPGHOME" "${XDG_DATA_HOME}/gnupg"
+var "GNUPG_CONF" "${XDG_CONFIG_HOME}/gnupg/gpg.conf"
+var "GNUPG_AGENT_CONF" "${XDG_CONFIG_HOME}/gnupg/gpg-agent.conf"
+var "DIRMNGR_CONF" "${XDG_CONFIG_HOME}/dirmngr/dirmngr.conf"
