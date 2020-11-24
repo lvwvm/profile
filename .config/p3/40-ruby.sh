274b2652378f272bc5d8e497233a924705c1a4ea
lvwvm <luwum@pm.me>
2020-11-24 14:03:51 -0600

    refactor(ruby): rework envvar setting.

--- .config/p3/40-ruby.sh
+++ .config/p3/40-ruby.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-if [ ! -d "${XDG_DATA_HOME}/gem" ]; then
-	mkdir "${XDG_DATA_HOME}/gem"
-fi
-
 var "GEM_HOME" "${XDG_DATA_HOME}/gem"
 var "PATH" "${PATH}:$(ruby -e 'print Gem.user_dir')/bin"
+[ ! -d "${XDG_DATA_HOME}/gem" ] && mkdir "${XDG_DATA_HOME}/gem"
