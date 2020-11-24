3ffdb0cf1a4a3d3d42e447e98e5f16b527dce554
lvwvm <luwum@pm.me>
2020-11-24 11:09:41 -0600

    feat(makepkg): essential makepkg variables.

--- .config/p3/15-arch.sh
+++ .config/p3/15-arch.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env sh
 
-# Where packages are built
+# Where to download source package files from AUR.
+var "SRCDEST" "${XDG_DATA_HOME}/src"
+[ ! -d "$SRCDEST" ] && mkdir "$SRCDEST"
+
+# Where to store packages after they have been built.
+var "PKGDEST" "${XDG_VAR_HOME}/aur"
+[ ! -d "$PKGDEST" ] && mkdir "$PKGDEST"
+
+# Where to store after packaging source files?
+var "SRCPKGDEST" "${HOME}/.local/dist"
+[ ! -d "$SRCPKGDEST" ] && mkdir $SRCPKGDEST
+
+# Where aur packages are built.
 var "BUILDDIR" "${XDG_BUILD_HOME}"
 [ ! -d "$BUILDDIR" ] && mkdir "$BUILDDIR"
 
-# Where to clone package build files
-var "AURDEST" "/var/cache/aur"
+# Where to clone package build files ( old pacaur variable )
+var "AURDEST" "${HOME}/Workspace"
+[ ! -d "$AURDEST" ] && mkdir "$AURDEST"
