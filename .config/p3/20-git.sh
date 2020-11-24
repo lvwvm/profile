087b04a51713386f9bfbd25b10d2df2aeb89127c
lvwvm <luwum@pm.me>
2020-11-24 11:34:58 -0600

    feat(git): GIT_ASKPASS set to user pinentry.

--- .config/p3/20-git.sh
+++ .config/p3/20-git.sh
@@ -3,4 +3,7 @@
 # user global location for git config
 [ ! -d ${XDG_CONFIG_HOME}/git ] && mkdir ${XDG_CONFIG_HOME}/git
 
+if has "pinentry"; then
+	var "GIT_ASKPASS" "$(command -v pinentry)"
+fi
 fi
