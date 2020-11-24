6f82dc4be3b5b39745710aa5918c603451e62237
lvwvm <luwum@pm.me>
2020-11-24 15:20:26 -0600

    feat(cc): define c compilers dynamically

--- .config/p3/10-cc.sh
+++ .config/p3/10-cc.sh
@@ -1,7 +1,47 @@
-# Compilers
-var "CC" "/usr/bin/gcc"
-var "CPP" "/usr/bin/cpp"
-var "CXX" "/usr/bin/g++"
+#!/usr/bin/env sh
+
+# Default C/C++ compilers
+if has "clang"; then
+
+	# Clang Compiler
+	var "CC" $(command -v clang)
+
+	# Clang++ Compiler
+	var "CXX" "${CC}++"
+
+	# C-Family Preprocessor
+	var "CPP" "${CC}-cpp"
+
+	var "HAS_CLANG" 1
+
+# Fallback to gcc if it exists.
+elif has "gcc"; then
+
+	# GCC Compiler
+	var "CC" $(command -v gcc)
+
+	# G++ Compiler
+	var "CXX" "${CC}++"
+
+	# GCC Preprocessor
+	var "CPP" $(command -v cpp)
+
+	var "HAS_GCC" 1
+else
+	err "unable to find default C compiler."
+	return 1
+fi
+
+# Set the version of the default compiler.
+if $HAS_CLANG; then
+	var "CLANG_VERSION" $(clang --version | awk 'NR==1 { print $3 }')
+	var "CLANG_INCLUDE_PATH" /usr/lib/clang/${CLANG_VERSION}/include
+fi
+
+if $HAS_GCC; then
+	var "GCC_VERSION" $(gcc --version | head -n 1 | cut -d" " -f3)
+	var "GCC_INCLUDE_PATH" /usr/lib/gcc/${GCC_VERSION}/include
+fi
 
 # Headers
 var "CPATH" "./include:/usr/local/include:/usr/lib/gcc/x86_64-pc-linux-gnu/8.3.0/include:/usr/lib/gcc/x86_64-pc-linux-gnu/8.3.0/include-fixed:/usr/include"
