d3475b86ff07966c3acbf9042ae5b3653b7668e8
lvwvm <luwum@pm.me>
2020-11-27 11:49:53 -0600

    refactor(pass): DRY up variables.

--- .config/p3/25-pass.sh
+++ .config/p3/25-pass.sh
@@ -2,7 +2,7 @@
 
 # PASSWORD_STORE_DIR - Overrides the default password storage directory.
 var "PASSWORD_STORE_DIR" "$XDG_DATA_HOME/pass"
-[ ! -d "$XDG_DATA_HOME/pass" ] && mkdir "$XDG_DATA_HOME/pass"
+[ ! -d "$PASSWORD_STORE_DIR" ] && mkdir "$PASSWORD_STORE_DIR"
 
 # PASSWORD_STORE_ENABLE_EXTENSIONS - This environment variable must be set to
 # "true" for extensions to be enabled.
@@ -11,4 +11,4 @@ var "PASSWORD_STORE_ENABLE_EXTENSIONS" "true"
 # PASSWORD_STORE_EXTENSIONS_DIR - The location to look for executable extension
 # files, by default PASSWORD_STORE_DIR/.extensions.
 var "PASSWORD_STORE_EXTENSIONS_DIR" "$PASSWORD_STORE_DIR/ext"
-[ ! -d "$PASSWORD_STORE_DIR/ext" ] && mkdir "$PASSWORD_STORE_DIR/ext"
+[ ! -d "$PASSWORD_STORE_EXTENSIONS_DIR" ] && mkdir "$PASSWORD_STORE_EXTENSIONS_DIR"
