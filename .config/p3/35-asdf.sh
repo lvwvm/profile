af829171553a65981b0cc9ccac0fa8a539319019
Idigo Luwum <luwum@pm.me>
2021-11-09 14:25:49 -0600

    fix(asdf,p3): rework asdf script.

--- .config/p3/35-asdf.sh
+++ .config/p3/35-asdf.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env sh
 
-# asdf configuration location.
-var "ASDF_CONFIG_FILE" "${XDG_CONFIG_HOME}/asdfrc"
-[ ! -f $ASDF_CONFIG_FILE ] && touch $ASDF_CONFIG_FILE
+install_asdf() {
+  [ ! -d $ASDF_DATA_DIR ] &&
+    git clone https://github.com/asdf-vm/asdf.git $ASDF_DATA_DIR --branch v0.8.1
+}
 
-# Location where asdf install plugins, shims and install.
+config_asdf() {
+  # asdf configuration location.
+  var "ASDF_CONFIG_FILE" "${XDG_CONFIG_HOME}/asdfrc"
+  [ ! -f $ASDF_CONFIG_FILE ] && touch $ASDF_CONFIG_FILE
+
+  # filename for storing the tool names and versions.
+  var "ASDF_DEFAULT_TOOL_VERSIONS_FILENAME" "${XDG_CONFIG_HOME}/asdf-versions"
+  var "ASDF_COMPLETIONS" "$ASDF_DATA_DIR/completions"
+}
+
+# file location asdf installs plugins, shims, etc.
 var "ASDF_DATA_DIR" "${XDG_DATA_HOME}/asdf"
-[ ! -d $ASDF_DATA_DIR ] &&
-	git clone https://github.com/asdf-vm/asdf.git $ASDF_DATA_DIR --branch v0.8.0
 
-# The name of the file storing the tool names and versions.
-var "ASDF_DEFAULT_TOOL_VERSIONS_FILENAME" "${XDG_CONFIG_HOME}/asdf-versions"
+if ! has "asdf"; then
+  install_asdf
+fi
 
-var "ASDF_COMPLETIONS" "$ASDF_DATA_DIR/completions"
+config_asdf
+
+unset install_asdf config_asdf
