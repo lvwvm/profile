ca08aa7e534afa26130eaf3a69b6264ae64789a4
Idigo Luwum <luwum@pm.me>
2022-01-03 13:12:51 -0600

    refactor(p3): awk path added

--- .config/p3/12-awk.sh
+++ .config/p3/12-awk.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-var "AWKPATH" ".:${XDG_DATA_HOME}/awk:/usr/share/awk"
-var "AWKLIBPATH" ".:${XDG_LIB_HOME}/awk:/usr/lib/gawk"
+var "AWKPATH" "${XDG_DATA_HOME}/awk:/usr/share/awk"
+var "AWKLIBPATH" "${XDG_LIB_HOME}/awk:/usr/lib/gawk:/usr/lib/awk"
--- .config/p3/99-terraform.sh
+++ .config/p3/99-terraform.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 var "TF_CLI_CONFIG_FILE" "${XDG_CONFIG_HOME}/terraformrc"
-[ ! -r ${TF_CLI_CONFIG_FILE} ] && touch ${TF_CLI_CONFIG_FILE}
+[ ! -r "${TF_CLI_CONFIG_FILE}" ] && touch "${TF_CLI_CONFIG_FILE}"
